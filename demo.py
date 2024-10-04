fffc9fce1ab818a970e184ad41a254606804c7b2
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:17 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,35 @@
+
+print("hello,world")
+print("hello"
+      "world")
+
+if 5>2:
+    print("five greater than two")
+    print("five greater than two")
+
+x=5
+y="ahaha"
+print (type(x))
+print(type(y))
+a=4
+A=4
+
+x=z=y="orange"
+print (x)
+print(y)
+print(z)
+x,y,z="python","is","awesome"
+print (x + y + z)
+
+a="hello"
+b='world'
+c=a+b
+print(c)
+
+
+
+a="hello,world!"
+print (a[6])
+
+b="hello,world"
+print(b[2:5])

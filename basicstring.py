1ff72f4deffa451aa823c114830f368806b923e9
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:08:20 +0530

    Add files via upload

---
+++ basicstring.py
@@ -0,0 +1,11 @@
+'''
+author:Ageena Mathew
+Date:18-10-24
+description:program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var="hello, world!"
+str=str_var[7:12]
+print(str)
+str_another=" Everyone!"
+str1=str+str_another
+print(str1)

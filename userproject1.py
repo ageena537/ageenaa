1d62a770e716430de1d6be5c3e7325c2a01d2655
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:08:51 +0530

    Add files via upload

---
+++ userproject1.py
@@ -0,0 +1,10 @@
+'''
+author:Ageena Mathew
+Date:18-10-24
+Description: program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+'''
+
+
+name=input("enter your name:")
+age=input("enter your age:")
+print("hello,",name+"!you are",age,"years old")

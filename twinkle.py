6e5fcb944b10c628aacc2333c7984112ae557836
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-25 16:11:29 +0530

    Add files via upload

---
+++ twinkle.py
@@ -0,0 +1,5 @@
+'''
+author:ageenA MATHEW
+date:18-10-24
+'''
+print("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t\t How I wonder what you are")

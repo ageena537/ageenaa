9a45fda7e1ba058c54fc095544f86aec79652588
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-25 14:39:35 +0530

    Add files via upload

---
+++ zeros.py
@@ -0,0 +1,12 @@
+'''
+author:ageena mathew
+date :25-10-24
+description:determine if a function is positive or negative
+'''
+num1=int(input("enter a number:"))
+if num1>0:
+    print("the given number,is positive")
+elif num1<0:
+    print("the given number is negative")
+else:
+    print(num1"is a zero")

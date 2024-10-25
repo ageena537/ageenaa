f4479789bfaae2ae81605af01cfce8db31df71a9
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-10-25 14:28:53 +0530

    Add files via upload

---
+++ greaterpositive.py
@@ -0,0 +1,10 @@
+'''
+author:ageena mathew
+date :25-10-24
+description:determine if a function is positive or negative
+'''
+num1=int(input("enter a number:"))
+if num1>0:
+    print("the given number is positive")
+else:
+    print("the given number is negative")

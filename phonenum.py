ce1857f4839f0637aac0af1aed4f616b0e3a91ad
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-11-29 14:29:47 +0530

    Add files via upload

--- phonenum.py
+++ phonenum.py
@@ -1,5 +1,10 @@
 phonenum=input("enter your number:")
 def number(phonenum):
+'''
+Function to checkif a mobile number is valid
+:param number:mobile number
+:return:True if number is valid,els false
+'''
     if len(phonenum)==10 and phonenum[0] in "987":
         return True
     return False
